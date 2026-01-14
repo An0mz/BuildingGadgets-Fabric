01b60278009a0d93138ab614ed2ac60b4cf3f0ba
Anomz <58313106+An0mz@users.noreply.github.com>
2026-01-14 15:01:41 -0500

    Fix copy render

--- src/main/java/com/direwolf20/buildinggadgets/client/renders/CopyPasteRender.java
+++ src/main/java/com/direwolf20/buildinggadgets/client/renders/CopyPasteRender.java
@@ -94,10 +94,19 @@ public class CopyPasteRender extends BaseRenderer implements IUpdateListener {
         int R = 255, G = 223, B = 127;
 
         MultiBufferSource.BufferSource buffer = Minecraft.getInstance().renderBuffers().bufferSource();
-        LevelRenderer.renderLineBox(matrix, buffer.getBuffer(OurRenderTypes.CopyGadgetLines), new AABB(region.getMin().getCenter(), region.getMax().offset(1, 1, 1).getCenter()), R / 255f, G / 255f, B / 255f, 1f);
-    }
+        
+        AABB box = new AABB(
+                region.getMin().getX(),
+                region.getMin().getY(),
+                region.getMin().getZ(),
+                region.getMax().getX() + 1,
+                region.getMax().getY() + 1,
+                region.getMax().getZ() + 1
+        );
 
-// In CopyPasteRender.java, update the renderPaste method:
+        LevelRenderer.renderLineBox(matrix, buffer.getBuffer(OurRenderTypes.CopyGadgetLines), box, R / 255f, G / 255f, B / 255f, 1f);
+        buffer.endBatch();
+    }
 
     private void renderPaste(PoseStack matrices, Vec3 cameraView, Player player, ItemStack heldItem) {
         Level world = player.level();
