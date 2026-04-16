0576af16f6d68e5281c72490de2f3095630f69a1
Anomz <58313106+An0mz@users.noreply.github.com>
2026-04-16 15:33:47 -0400

    Fix problem from other version

--- src/main/java/com/direwolf20/buildinggadgets/client/screen/CopyGUI.java
+++ src/main/java/com/direwolf20/buildinggadgets/client/screen/CopyGUI.java
@@ -94,7 +94,7 @@ public class CopyGUI extends Screen {
     }
 
     private void drawFieldLabel(GuiGraphics guiGraphics, String name, int x, int y) {
-        guiGraphics.drawString(Minecraft.getInstance().font, name, this.x + x, this.y + y, 0xFFFFFF, true);
+        guiGraphics.drawString(Minecraft.getInstance().font, name, this.x + x, this.y + y, 0xFFFFFFFF, true);
     }
 
     private void coordsModeSwitch() {
@@ -124,6 +124,8 @@ public class CopyGUI extends Screen {
 
     @Override
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
+        super.render(guiGraphics, mouseX, mouseY, partialTicks);
+
         drawFieldLabel(guiGraphics, GuiTranslation.FIELD_START.format() + " X", -175, -36);
         drawFieldLabel(guiGraphics, "Y", -45, -36);
         drawFieldLabel(guiGraphics, "Z", 55, -36);
@@ -131,10 +133,8 @@ public class CopyGUI extends Screen {
         drawFieldLabel(guiGraphics, "Y", -45, -11);
         drawFieldLabel(guiGraphics, "Z", 55, -11);
 
-        guiGraphics.drawCenteredString(Minecraft.getInstance().font, I18n.get(GuiTranslation.COPY_LABEL_HEADING.getTranslationKey()), this.x, this.y - 80, 0xFFFFFF);
-        guiGraphics.drawCenteredString(Minecraft.getInstance().font, I18n.get(GuiTranslation.COPY_LABEL_SUBHEADING.getTranslationKey()), this.x, this.y - 68, 0xFFFFFF);
-
-        super.render(guiGraphics, mouseX, mouseY, partialTicks);
+        guiGraphics.drawCenteredString(Minecraft.getInstance().font, I18n.get(GuiTranslation.COPY_LABEL_HEADING.getTranslationKey()), this.x, this.y - 80, 0xFFFFFFFF);
+        guiGraphics.drawCenteredString(Minecraft.getInstance().font, I18n.get(GuiTranslation.COPY_LABEL_SUBHEADING.getTranslationKey()), this.x, this.y - 68, 0xFFFFFFFF);
     }
 
     @Override
