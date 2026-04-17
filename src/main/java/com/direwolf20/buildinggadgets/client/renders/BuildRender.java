a9a1bff20e830215706cd287bb70e6d6fb87e839
Anomz <58313106+An0mz@users.noreply.github.com>
2026-04-16 22:57:20 -0400

    Fix anchor crash

--- src/main/java/com/direwolf20/buildinggadgets/client/renders/BuildRender.java
+++ src/main/java/com/direwolf20/buildinggadgets/client/renders/BuildRender.java
@@ -76,13 +76,13 @@ public class BuildRender extends BaseRenderer {
             errorState = null;
         }
 
-        List<BlockPos> coordinates = anchor.orElseGet(() -> {
+        List<BlockPos> coordinates = new ArrayList<>(anchor.orElseGet(() -> {
             AbstractMode mode = !this.isExchanger ? GadgetBuilding.getToolMode(heldItem).getMode() : GadgetExchanger.getToolMode(heldItem).getMode();
             return mode.getCollection(
                     new AbstractMode.UseContext(player.level(), renderBlockState, lookingAt.getBlockPos(), heldItem, lookingAt.getDirection(), !this.isExchanger && GadgetBuilding.shouldPlaceAtop(heldItem), GadgetBuilding.getConnectedArea(heldItem)),
                     player
             );
-        });
+        }));
 
         BlockPos targetPos = lookingAt.getBlockPos();
         coordinates.sort(Comparator.comparingDouble(pos -> pos.distSqr(targetPos)));
